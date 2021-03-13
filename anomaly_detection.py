afdc47d637110789aa78291afb29795b546fe1bb
alpyesilgul <70238558+alpyesilgul@users.noreply.github.com>
2021-03-13 23:20:36 +0300

    Update anomaly_detection.py

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -3,9 +3,9 @@ from utils import load_dataset
 
 
 def model(dataset):
-    print("[INFO] preparing dataset...")
+    print("Preparing dataset...")
     data = load_dataset(dataset, bins=(3, 3, 3))
-    print("[INFO] fitting anomaly detection model...")
+    print("Fitting anomaly detection model...")
     model = IsolationForest(n_estimators=100, contamination=0.01,
                             random_state=42)
     model.fit(data)
